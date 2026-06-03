894aec88d235d63924f4ab1122b5402f2d0c9eb4
KaiButt <kyycowpig@gmail.com>
2026-06-02 21:01:27 -0400

    Blaze costs 2 more

--- jokers/Blaze.lua
+++ jokers/Blaze.lua
@@ -28,7 +28,7 @@ SMODS.Joker {
     },
     pos = { x = 0, y = 0 },
     rarity = 1,
-    cost = 3,
+    cost = 5,
     loc_vars = function(self, info_queue, center)
         return { vars = { center.ability.extra.handPerRound} }
     end,
