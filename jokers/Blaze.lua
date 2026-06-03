d75ecab0518df5d93e98ca434afbb65ea8b42165
KaiButt <kyycowpig@gmail.com>
2026-06-02 21:00:42 -0400

    Blaze buff

--- jokers/Blaze.lua
+++ jokers/Blaze.lua
@@ -11,6 +11,7 @@ SMODS.Joker {
     loc_txt = {
         name = 'Blaze',
         text = {
+            '{C:blue}+#1#{} hand per round',
             'Every card you {C:attention}play{}',
             'Will be {C:attention}shuffled back{} into your {C:attention}deck{}',
         },
@@ -20,11 +21,28 @@ SMODS.Joker {
     },
     name = "Blaze",
     atlas = "Blaze",
+    config = {
+        extra = {
+            handPerRound = 1
+        },
+    },
     pos = { x = 0, y = 0 },
     rarity = 1,
     cost = 3,
+    loc_vars = function(self, info_queue, center)
+        return { vars = { center.ability.extra.handPerRound} }
+    end,
+    add_to_deck = function(self, card, from_debuff)
+        G.GAME.round_resets.hands = G.GAME.round_resets.hands + card.ability.extra.handPerRound
+        ease_hands_played(card.ability.extra.handPerRound)
+    end,
+    remove_from_deck = function(self, card, from_debuff)
+        G.GAME.round_resets.hands = G.GAME.round_resets.hands - card.ability.extra.handPerRound
+        ease_hands_played(-card.ability.extra.handPerRound)
+    end,
     blueprint_compat = false,
     pools = { ["goob"] = true},
+
     calculate = function(self, card, context)
         if context.stay_flipped and context.from_area == G.play then
             return {
