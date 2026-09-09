cf535089fcf80fb8b4500d6af639cc2853e87edb
KaiButt <kyycowpig@gmail.com>
2026-09-08 22:29:33 -0400

    removed debug condition on jobros

--- content/achievements/Achievements.lua
+++ content/achievements/Achievements.lua
@@ -3,7 +3,6 @@ SMODS.Achievement({
     hidden_name = true,
     hidden_text = false,
     bypass_all_unlocked = false,
-    reset_on_startup = true,
 	unlock_condition = function(self, args)
         if args.type == 'win' then
             return next(SMODS.find_card('j_goob_Jobingles')) and next(SMODS.find_card('j_goob_Jobangles'))
